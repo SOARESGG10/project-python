3d76284e3193c8e57e175f25807487c4b4867f8a
Rafael Henrique <spectggof@gmail.com>
2022-12-27 12:58:23 -0300

    Added "clean entry system"

--- src/layout.py
+++ src/layout.py
@@ -1,5 +1,8 @@
 import PySimpleGUI as sg
-from settings import layout_title, layout_input, layout_button_submit, layout_title_input, paht_icon
+from settings import layout_title, layout_input, layout_button_submit, layout_title_input, paht_icon, validate
+
+regex = "^[+]?([0-9](\d{0,11}))?$"
+old = {"IN1": ""}
 
 layout = [
     [layout_title],
@@ -7,11 +10,22 @@ layout = [
     [layout_button_submit]
 ]
 
-window = sg.Window("Validador de CPF", layout, icon=paht_icon)
+window = sg.Window("Validador de CPF", layout, icon=paht_icon, finalize=True)
+
 
 while True:
     event, values = window.read()
-
     if event == sg.WINDOW_CLOSED:
         break
-
+    elif event == "IN1":
+        element, text = window[event], values[event]
+        if validate(regex, text):
+            try:
+                if len(text) > 11:
+                    element.update(old[event])
+                    continue
+            except ValueError as e:
+                pass
+            old[event] = text
+        else:
+            element.update(old[event])
--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,18 @@
 import PySimpleGUI as sg
+import re
+
+
+def validate(regex, text):
+    result = re.search(regex, text)
+    return False if result is None or result.group() != text else True
+
+
 icon = "../public/img/compass.svg"
 
 sg.theme("Reddit")
 layout_title = sg.Text("Validador de CPF's", size=22, font=(0, 18), text_color="#1F6FEB",
                        justification="center", pad=(0, 10))
 layout_title_input = sg.Text("Informe um CPF:", font=10)
-layout_input = sg.Input(size=17, font=10, key="input", enable_events=True)
+layout_input = sg.Input(size=17, font=10, key="IN1", enable_events=True)
 layout_button_submit = sg.Button("Verificar", size=43, pad=(0, 10))
 paht_icon = "../public/img/approved.png"
